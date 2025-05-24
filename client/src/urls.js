f10a53eaef6d02d44836d2e45f9c106c2d8b0081
nikhil94058 <kumarnikhil94058@gmail.com>
2025-05-24 22:17:44 +0530

    Committing for deploying

--- client/src/urls.js
+++
@@ -1,2 +0,0 @@
-export const base_url1 = `http://localhost:8000`;
-export const base_url = `https://landrisebackend.onrender.com`;
--- server/routes/genRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reviewController = require('../controllers/genController');
-
-router.post('/feedback/upload', reviewController.submitReview);
-module.exports = router;
